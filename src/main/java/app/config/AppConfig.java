fca1f2a8e22e244a18691fc378387a7f79c74cb1
LeThanhPhuc <phuclethanh2906@gmail.com>
2025-05-26 23:45:15 +0700

    Cập nhật cấu hình, code nguồn và assets

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,10 +1,12 @@
 package app.config;
 
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.context.annotation.*;
+import app.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.*;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -13,44 +15,47 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
+@ComponentScan(basePackages = {"app.service", "app.dao"})
+@EnableJpaRepositories(basePackages = "app.repository")
 @EnableTransactionManagement
-@ComponentScan(basePackages = "app")                   // quét service/dao
-@EnableJpaRepositories(basePackages = "app.repository")// quét repository
 public class AppConfig {
 
-    /* 1 ▪ DataSource */
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource ds = new DriverManagerDataSource();
-        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/chatdb?serverTimezone=UTC&useSSL=false");
-        ds.setUsername("root");
+        org.springframework.jdbc.datasource.DriverManagerDataSource ds =
+                new org.springframework.jdbc.datasource.DriverManagerDataSource();
+
+        ds.setDriverClassName(Config.getDbDriver());
+        ds.setUrl(Config.getDbUrl());
+        ds.setUsername(Config.getDbUsername());
+        ds.setPassword(Config.getDbPassword());
+
         return ds;
     }
 
-    /* 2 ▪ EntityManagerFactory  (tên MẶC ĐỊNH = entityManagerFactory) */
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-            DataSource ds) {
-
-        LocalContainerEntityManagerFactoryBean f =
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emf =
                 new LocalContainerEntityManagerFactoryBean();
-        f.setDataSource(ds);
-        f.setPackagesToScan("app.model");          // entity package
-        f.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 
-        Properties jpa = new Properties();
-        jpa.put("hibernate.hbm2ddl.auto", "update");  // tạo bảng tự động
-        jpa.put("hibernate.show_sql",      "false");
-        jpa.put("hibernate.format_sql",    "false");
-        f.setJpaProperties(jpa);
-        return f;
+        emf.setDataSource(dataSource());
+        emf.setPackagesToScan("app.model");
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties props = new Properties();
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        props.setProperty("jakarta.persistence.jdbc.url", Config.getDbUrl());
+        props.setProperty("jakarta.persistence.jdbc.user", Config.getDbUsername());
+        props.setProperty("jakarta.persistence.jdbc.password", Config.getDbPassword());
+        props.setProperty("jakarta.persistence.jdbc.driver", Config.getDbDriver());
+
+        emf.setJpaProperties(props);
+
+        return emf;
     }
 
-    /* 3 ▪ TransactionManager (Spring sẽ tự tiêm entityManagerFactory) */
     @Bean
-    public PlatformTransactionManager transactionManager(
-            EntityManagerFactory emf) {
-        return new JpaTransactionManager(emf);
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager(entityManagerFactory().getObject());
     }
 }
--- src/main/java/app/util/Config.java
+++ src/main/java/app/util/Config.java
@@ -1,49 +1,76 @@
 package app.util;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final Properties props = new Properties();
+    private static final Properties properties = new Properties();
 
     static {
-        try (InputStream is = Config.class.getResourceAsStream("/config.properties")) {
-            if (is != null) {
-                props.load(is);
+        try {
+            // Đọc từ classpath thay vì file system
+            InputStream input = Config.class.getClassLoader()
+                    .getResourceAsStream("config.properties");
+
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                // Sử dụng giá trị mặc định
+                setDefaultProperties();
             } else {
-                System.err.println("Warning: config.properties not found, using defaults");
+                properties.load(input);
+                input.close();
             }
-        } catch (IOException e) {
-            System.err.println("Warning: Error loading config.properties, using defaults");
+        } catch (Exception e) {
             e.printStackTrace();
+            setDefaultProperties();
         }
+    }
 
-        // Ưu tiên thuộc tính hệ thống (System properties)
-        String serverHost = System.getProperty("server.host");
-        String serverPort = System.getProperty("server.port");
-        String clientHost = System.getProperty("client.host");
-        String clientPort = System.getProperty("client.port");
+    private static void setDefaultProperties() {
+        // Giá trị mặc định nếu không đọc được file
+        properties.setProperty("server.host", "0.0.0.0");
+        properties.setProperty("server.port", "5000");
+        properties.setProperty("client.host", "13.229.231.49");
+        properties.setProperty("client.port", "5000");
+        properties.setProperty("db.url", "jdbc:mysql://13.229.231.49:3306/chatdb?serverTimezone=UTC");
+        properties.setProperty("db.username", "chatuser");
+        properties.setProperty("db.password", "123456");
+        properties.setProperty("db.driver", "com.mysql.cj.jdbc.Driver");
+    }
 
-        if (serverHost != null) props.setProperty("server.host", serverHost);
-        if (serverPort != null) props.setProperty("server.port", serverPort);
-        if (clientHost != null) props.setProperty("client.host", clientHost);
-        if (clientPort != null) props.setProperty("client.port", clientPort);
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public static String getDbUrl() {
+        return properties.getProperty("db.url");
+    }
+
+    public static String getDbUsername() {
+        return properties.getProperty("db.username");
+    }
+
+    public static String getDbPassword() {
+        return properties.getProperty("db.password");
+    }
+
+    public static String getDbDriver() {
+        return properties.getProperty("db.driver");
     }
 
     public static String getServerHost() {
-        return props.getProperty("server.host", "localhost");
+        return properties.getProperty("server.host", "0.0.0.0");
     }
 
     public static int getServerPort() {
-        return Integer.parseInt(props.getProperty("server.port", "5000"));
+        return Integer.parseInt(properties.getProperty("server.port", "5000"));
     }
 
     public static String getClientHost() {
-        return props.getProperty("client.host", "localhost");
+        return properties.getProperty("client.host", "localhost");
     }
 
     public static int getClientPort() {
-        return Integer.parseInt(props.getProperty("client.port", "5000"));
+        return Integer.parseInt(properties.getProperty("client.port", "5000"));
     }
 }
